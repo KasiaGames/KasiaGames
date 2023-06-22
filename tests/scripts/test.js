2a167b0b43994d0f30a652bcca965d6fa3cd6cf1
youri <youridevrije05@gmail.com>
2023-06-22 19:22:06 +0200

    Update test.js

--- tests/scripts/test.js
+++ tests/scripts/test.js
@@ -1,29 +1,3 @@
-/*var contentRowBox = document.querySelectorAll(".contentRowBox");
-
-var textBoxHalf = contentRowBox[0].querySelectorAll(".textBoxHalf");
-var decorationFiller = textBoxHalf[0].querySelectorAll(".decorationFiller");
-
-/*for (var contentRowBoxs = 0; contentRowBoxs < contentRowBox.length; contentRowBoxs++){
-    console.log("contentRowBox" + contentRowBoxs);
-
-    for (var textBoxHalfs = 0; textBoxHalfs < textBoxHalf.length; textBoxHalfs++){
-        console.log("textBoxHalf" + textBoxHalfs);
-
-        for (var decorationFillers = 0; decorationFillers < decorationFiller.length; decorationFillers++){
-            console.log("decorationFiller" + decorationFillers);
-
-            var width = decorationFiller[decorationFillers].offsetWidth;
-            var heigth = decorationFiller[decorationFillers].offsetHeight;
-            if (heigth > width){
-            decorationFiller[decorationFillers].style.backgroundColor = "red";
-            }
-            else {
-            decorationFiller[decorationFillers].style.backgroundColor = "blue";
-            }
-    
-        }
-    }
-}*/
 var decorationFiller = document.querySelectorAll(".decorationFiller");
 
 for (var decorationFillers = 0; decorationFillers < decorationFiller.length; decorationFillers++){
