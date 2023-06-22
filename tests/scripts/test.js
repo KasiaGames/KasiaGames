58b1b4dd578a1ae965b4f20f1bb6d98f831eb3c8
youri <youridevrije05@gmail.com>
2023-06-22 18:23:02 +0200

    things

--- tests/scripts/test.js
+++ tests/scripts/test.js
@@ -4,17 +4,23 @@ var textBoxHalf = contentRowBox[0].querySelectorAll(".textBoxHalf");
 var decorationFiller = textBoxHalf[0].querySelectorAll(".decorationFiller");
 
 for (var contentRowBoxs = 0; contentRowBoxs < contentRowBox.length; contentRowBoxs++){
-for (var textBoxHalfs = 0; textBoxHalfs < textBoxHalf.length; textBoxHalfs++){
-for (var decorationFillers = 0; decorationFillers < decorationFiller.length; decorationFillers++){
-    var width = decorationFiller[decorationFillers].offsetWidth;
-    var heigth = decorationFiller[decorationFillers].offsetHeight;
-    if (heigth > width){
-        decorationFiller[decorationFillers].style.backgroundColor = "red";
-    }
-    else {
-        decorationFiller[decorationFillers].style.backgroundColor = "blue";
-    }
+    console.log(contentRowBoxs);
+
+    for (var textBoxHalfs = 0; textBoxHalfs < textBoxHalf.length; textBoxHalfs++){
+        console.log(textBoxHalfs);
+
+        for (var decorationFillers = 0; decorationFillers < decorationFiller.length; decorationFillers++){
+            console.log(decorationFillers);
+
+            var width = decorationFiller[decorationFillers].offsetWidth;
+            var heigth = decorationFiller[decorationFillers].offsetHeight;
+            if (heigth > width){
+            decorationFiller[decorationFillers].style.backgroundColor = "red";
+            }
+            else {
+            decorationFiller[decorationFillers].style.backgroundColor = "blue";
+            }
     
-}
-}
+        }
+    }
 }
