9169a2a435b5e6d558fbcdd6feb111c44882bfdc
youri <youridevrije05@gmail.com>
2023-06-24 13:44:07 +0200

    add more image sizes

--- website/baseStyle/scripts/decorationFiller.js
+++ website/baseStyle/scripts/decorationFiller.js
@@ -2,10 +2,12 @@
 var decorationFiller = document.querySelectorAll(".decorationFiller");
 
 //the size diverence it will accept for placing red or green squares
-var acceptedDiverence = 0.4;
+var maxAcceptedDiverence = 0.3;
+var AcceptedDiverence = 0.5;
+var minAcceptedDiverence = 0.4;
 
 //the amount of backgrounds
-var backgrounds = 3;
+var backgrounds = 1;
 
 //reloads the page on resize to make sure the squares always have the correct color
 window.addEventListener('resize', function () { 
@@ -16,30 +18,38 @@ window.addEventListener('resize', function () {
 window.onload = function() {
 //loops trough the list
 for (var decorationFillers = 0; decorationFillers < decorationFiller.length; decorationFillers++){
-    console.log("decorationFiller" + decorationFillers);
 
     //the width and heigth of a decorationFiller
     var width = decorationFiller[decorationFillers].offsetWidth;
     var height = decorationFiller[decorationFillers].offsetHeight;
     
     //decides the color of a decorationFiller
-    //tall decorationFillers will become red
-    //wide decorationFillers will become green
-    //but decorationFillers that are too wide or tall will become green
-    if (height > width){
+    //extra tall
+    if (maxAcceptedDiverence * height > width){
+    decorationFiller[decorationFillers].style.background = "url(../baseStyle/baseImage/backgroundDecorationImages/extraTall" + (Math.floor(Math.random() * backgrounds + 1) + ".png)");
+    decorationFiller[decorationFillers].style.backgroundRepeat="no-repeat";
+    decorationFiller[decorationFillers].style.backgroundSize = "100%";
+    }
+    //tall
+    else if (AcceptedDiverence * height > width && width > maxAcceptedDiverence * height){
     decorationFiller[decorationFillers].style.background = "url(../baseStyle/baseImage/backgroundDecorationImages/tall" + (Math.floor(Math.random() * backgrounds + 1) + ".png)");
     decorationFiller[decorationFillers].style.backgroundRepeat="no-repeat";
-    decorationFiller[decorationFillers].style.backgroundSize = "contain";
+    decorationFiller[decorationFillers].style.backgroundSize = "100%";
     }
-    else if (height < width && height > acceptedDiverence * width){
-    decorationFiller[decorationFillers].style.background = "url(../baseStyle/baseImage/backgroundDecorationImages/wide" + (Math.floor(Math.random() * backgrounds + 1) + ".png)");
+    //short
+    else if (height > minAcceptedDiverence * width && width > height){
+    decorationFiller[decorationFillers].style.background = "url(../baseStyle/baseImage/backgroundDecorationImages/short" + (Math.floor(Math.random() * backgrounds + 1) + ".png)");
     decorationFiller[decorationFillers].style.backgroundRepeat="no-repeat";
-    decorationFiller[decorationFillers].style.backgroundSize = "contain";
+    decorationFiller[decorationFillers].style.backgroundSize = "100%";
+    }
+    //medium
+    else if (height > AcceptedDiverence * width && width > AcceptedDiverence * height) {
+    decorationFiller[decorationFillers].style.background = "url(../baseStyle/baseImage/backgroundDecorationImages/medium" + (Math.floor(Math.random() * backgrounds + 1) + ".png)");
+    decorationFiller[decorationFillers].style.backgroundRepeat="no-repeat";
+    decorationFiller[decorationFillers].style.backgroundSize = "100%";
     }
     else {
-    decorationFiller[decorationFillers].style.background = "url(../baseStyle/baseImage/backgroundDecorationImages/empty.png)";
-    decorationFiller[decorationFillers].style.backgroundRepeat="no-repeat";
-    decorationFiller[decorationFillers].style.backgroundSize = "contain";
+
     }
 
 }
